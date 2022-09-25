8987effff156893f9140b8c49b45c08c5984e1c5
jotty <bard143games@gmail.com>
2022-09-25 22:59:27 +0300

    about section from json and some little changes

---
+++ js/home.js
@@ -0,0 +1,8 @@
+import {loadJSON} from "/js/index.js";
+
+let about_section = document.getElementById("about-me-inner");
+
+loadJSON('/js/json/index.json', response => {
+    let json = JSON.parse(response);
+    about_section.innerText = json["about"];
+});
--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-let projectsListHtml = document.getElementsByClassName("projects-list")[0]
-
-function loadJSON(file, callback) {
+export function loadJSON(file, callback) {
     let xObj = new XMLHttpRequest();
     xObj.overrideMimeType("application/json");
     xObj.open('GET', file, true);
@@ -11,23 +9,3 @@ function loadJSON(file, callback) {
     };
     xObj.send(null);
 }
-
-loadJSON("/js/projects.json", response => {
-    let json = JSON.parse(response);
-    json.forEach(elem => {
-        let newDiv = document.createElement('div');
-        newDiv.className = "row project";
-
-        let projectNameDiv = document.createElement('div');
-        projectNameDiv.className = "row project-name";
-        projectNameDiv.innerText = elem["name"];
-        newDiv.appendChild(projectNameDiv);
-
-        let projectDescriptionDiv = document.createElement('div');
-        projectDescriptionDiv.className = "row project-description";
-        projectDescriptionDiv.innerText = elem["description"];
-        newDiv.appendChild(projectDescriptionDiv);
-
-        projectsListHtml.appendChild(newDiv);
-    })
-})
